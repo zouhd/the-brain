f1700b1013894a7e1380612185de888765414d79
zouhd <haodongzou@163.com>
2016-10-16 23:36:18 +0800

    新建FxDemo

---
+++ FenxingDemo/FxImage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FenxingDemo
+{
+    class FxImage
+    {
+        private String m_base_dir;//存放分形图片的路径
+        private String m_image_name;//分形图片名称,A1_001.jpg,A1表示分形题号，001表示分形截图。
+        private int m_seq_num;//显示编号，标记图片显示顺序
+        private int m_fx_x;//分形函数参数x
+        private int m_fx_y;//分形函数参数y
+        private Boolean m_show_flag;/*显示标记，标记图片是否显示，不显示的图片
+       包括开头，结尾，选手选择的3张截图以及电脑随机消除的95张截图*/
+        private Boolean m_study_flag;//是否被选手选中学习
+
+        public FxImage(String base_dir, String image_name, int seq_num, int fx_x, int fx_y)
+        {
+            m_base_dir = base_dir;
+            m_image_name = image_name;
+            m_seq_num = seq_num;
+            m_fx_x = fx_x;
+            m_fx_y = fx_y;
+
+            m_show_flag = true;//默认显示
+            m_study_flag = false;//默认没被选手选中学习
+        }
+
+        public int SEQ_NUM
+        {
+            get { return m_seq_num; }
+            set { m_seq_num = value; }
+        }
+
+        public Boolean SHOW_FLAG
+        {
+            get { return m_show_flag; }
+            set { m_show_flag = value; }
+        }
+
+        public int FX_X
+        {
+            get { return m_fx_x; }
+            set { m_fx_x = value; }
+        }
+
+        public int FX_Y
+        {
+            get { return m_fx_y; }
+            set { m_fx_y = value; }
+        }
+
+        /*
+         * 显示该图片
+         */
+        public void ShowImage()
+        {
+
+        }
+    }
+}
---
+++ FenxingDemo/FxImageList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace FenxingDemo
+{
+    class FxImageList
+    {
+        private List<FxImage> m_fx_list;
+        private int m_fx_image_num;//分形图片数量
+
+        /* *
+         * 构造函数，传入分形图片的数量，默认250图片
+         * */
+        public FxImageList(int fx_image_num = 200)
+        {
+            m_fx_image_num = fx_image_num;
+            m_fx_list = new List<FxImage>(m_fx_image_num);
+
+        }
+
+
+        /**
+         * 
+         *将分形图片打乱展示 
+         **/
+        public void ShuffleImages()
+        {
+
+        }
+
+        /**
+         * 显示分形图片
+         * */
+        public void ShowImages()
+        {
+        }
+
+        /***
+         *放大显示图片 
+         * */
+        public void MaginifyImage(String image_name)
+        {
+        }
+
+
+
+    }
+}
