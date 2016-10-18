53cbb454422247e2c22e3255474c6392713af3cc
zhangyang01123 <zhangyang01123@gmail.com>
2016-10-19 00:20:13 +0100

    change the page operation logic (button and label) from BigScreen to console screen.

--- FenXingFormDemo/BigScreen.Designer.cs
+++ FenXingFormDemo/BigScreen.Designer.cs
@@ -28,14 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button_pre = new System.Windows.Forms.Button();
-            this.button_next = new System.Windows.Forms.Button();
-            this.label_page = new System.Windows.Forms.Label();
-            this.textBox_page_num = new System.Windows.Forms.TextBox();
-            this.button_select = new System.Windows.Forms.Button();
-            this.textBox_pic_num = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -60,127 +52,56 @@
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button_pre
-            // 
-            this.button_pre.Location = new System.Drawing.Point(602, 521);
-            this.button_pre.Name = "button_pre";
-            this.button_pre.Size = new System.Drawing.Size(75, 23);
-            this.button_pre.TabIndex = 1;
-            this.button_pre.Text = "上一页";
-            this.button_pre.UseVisualStyleBackColor = true;
-            this.button_pre.Click += new System.EventHandler(this.button_pre_Click);
-            // 
-            // button_next
-            // 
-            this.button_next.Location = new System.Drawing.Point(683, 521);
-            this.button_next.Name = "button_next";
-            this.button_next.Size = new System.Drawing.Size(75, 23);
-            this.button_next.TabIndex = 2;
-            this.button_next.Text = "下一页";
-            this.button_next.UseVisualStyleBackColor = true;
-            this.button_next.Click += new System.EventHandler(this.button_next_Click);
-            // 
-            // label_page
-            // 
-            this.label_page.AutoSize = true;
-            this.label_page.Location = new System.Drawing.Point(545, 526);
-            this.label_page.Name = "label_page";
-            this.label_page.Size = new System.Drawing.Size(41, 12);
-            this.label_page.TabIndex = 3;
-            this.label_page.Text = "label1";
-            // 
-            // textBox_page_num
-            // 
-            this.textBox_page_num.Location = new System.Drawing.Point(70, 523);
-            this.textBox_page_num.Name = "textBox_page_num";
-            this.textBox_page_num.Size = new System.Drawing.Size(100, 21);
-            this.textBox_page_num.TabIndex = 4;
-            // 
-            // button_select
-            // 
-            this.button_select.Location = new System.Drawing.Point(348, 521);
-            this.button_select.Name = "button_select";
-            this.button_select.Size = new System.Drawing.Size(75, 23);
-            this.button_select.TabIndex = 5;
-            this.button_select.Text = "选择";
-            this.button_select.UseVisualStyleBackColor = true;
-            this.button_select.Click += new System.EventHandler(this.button_select_Click);
-            // 
-            // textBox_pic_num
-            // 
-            this.textBox_pic_num.Location = new System.Drawing.Point(236, 523);
-            this.textBox_pic_num.Name = "textBox_pic_num";
-            this.textBox_pic_num.Size = new System.Drawing.Size(100, 21);
-            this.textBox_pic_num.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(182, 526);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "图片编号";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 526);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "页数";
-            // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(10, 20);
+            this.pictureBox1.Location = new System.Drawing.Point(10, 22);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Padding = new System.Windows.Forms.Padding(5);
-            this.pictureBox1.Size = new System.Drawing.Size(130, 170);
+            this.pictureBox1.Size = new System.Drawing.Size(130, 184);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(160, 20);
+            this.pictureBox2.Location = new System.Drawing.Point(160, 22);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Padding = new System.Windows.Forms.Padding(5);
-            this.pictureBox2.Size = new System.Drawing.Size(130, 170);
+            this.pictureBox2.Size = new System.Drawing.Size(130, 184);
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(310, 20);
+            this.pictureBox3.Location = new System.Drawing.Point(310, 22);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Padding = new System.Windows.Forms.Padding(5);
-            this.pictureBox3.Size = new System.Drawing.Size(130, 170);
+            this.pictureBox3.Size = new System.Drawing.Size(130, 184);
             this.pictureBox3.TabIndex = 0;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(460, 20);
+            this.pictureBox4.Location = new System.Drawing.Point(460, 22);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Padding = new System.Windows.Forms.Padding(5);
-            this.pictureBox4.Size = new System.Drawing.Size(130, 170);
+            this.pictureBox4.Size = new System.Drawing.Size(130, 184);
             this.pictureBox4.TabIndex = 0;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(610, 20);
+            this.pictureBox5.Location = new System.Drawing.Point(610, 22);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Padding = new System.Windows.Forms.Padding(5);
-            this.pictureBox5.Size = new System.Drawing.Size(130, 170);
+            this.pictureBox5.Size = new System.Drawing.Size(130, 184);
             this.pictureBox5.TabIndex = 0;
             this.pictureBox5.TabStop = false;
             // 
             // label_param1
             // 
             this.label_param1.AutoSize = true;
-            this.label_param1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_param1.Location = new System.Drawing.Point(44, 165);
+            this.label_param1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_param1.Location = new System.Drawing.Point(44, 179);
             this.label_param1.Name = "label_param1";
             this.label_param1.Size = new System.Drawing.Size(55, 21);
             this.label_param1.TabIndex = 1;
@@ -189,8 +110,8 @@
             // label_fx1
             // 
             this.label_fx1.AutoSize = true;
-            this.label_fx1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_fx1.Location = new System.Drawing.Point(48, 193);
+            this.label_fx1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_fx1.Location = new System.Drawing.Point(48, 209);
             this.label_fx1.Name = "label_fx1";
             this.label_fx1.Size = new System.Drawing.Size(55, 21);
             this.label_fx1.TabIndex = 1;
@@ -199,8 +120,8 @@
             // label_fx2
             // 
             this.label_fx2.AutoSize = true;
-            this.label_fx2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_fx2.Location = new System.Drawing.Point(198, 193);
+            this.label_fx2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_fx2.Location = new System.Drawing.Point(198, 209);
             this.label_fx2.Name = "label_fx2";
             this.label_fx2.Size = new System.Drawing.Size(55, 21);
             this.label_fx2.TabIndex = 1;
@@ -209,8 +130,8 @@
             // label_fx3
             // 
             this.label_fx3.AutoSize = true;
-            this.label_fx3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_fx3.Location = new System.Drawing.Point(348, 193);
+            this.label_fx3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_fx3.Location = new System.Drawing.Point(348, 209);
             this.label_fx3.Name = "label_fx3";
             this.label_fx3.Size = new System.Drawing.Size(55, 21);
             this.label_fx3.TabIndex = 1;
@@ -219,8 +140,8 @@
             // label_fx4
             // 
             this.label_fx4.AutoSize = true;
-            this.label_fx4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_fx4.Location = new System.Drawing.Point(498, 193);
+            this.label_fx4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_fx4.Location = new System.Drawing.Point(498, 209);
             this.label_fx4.Name = "label_fx4";
             this.label_fx4.Size = new System.Drawing.Size(55, 21);
             this.label_fx4.TabIndex = 1;
@@ -229,8 +150,8 @@
             // label_fx5
             // 
             this.label_fx5.AutoSize = true;
-            this.label_fx5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_fx5.Location = new System.Drawing.Point(648, 193);
+            this.label_fx5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_fx5.Location = new System.Drawing.Point(648, 209);
             this.label_fx5.Name = "label_fx5";
             this.label_fx5.Size = new System.Drawing.Size(55, 21);
             this.label_fx5.TabIndex = 1;
@@ -239,8 +160,8 @@
             // label_param2
             // 
             this.label_param2.AutoSize = true;
-            this.label_param2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_param2.Location = new System.Drawing.Point(194, 165);
+            this.label_param2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_param2.Location = new System.Drawing.Point(194, 179);
             this.label_param2.Name = "label_param2";
             this.label_param2.Size = new System.Drawing.Size(55, 21);
             this.label_param2.TabIndex = 1;
@@ -249,8 +170,8 @@
             // label_param3
             // 
             this.label_param3.AutoSize = true;
-            this.label_param3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_param3.Location = new System.Drawing.Point(344, 165);
+            this.label_param3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_param3.Location = new System.Drawing.Point(344, 179);
             this.label_param3.Name = "label_param3";
             this.label_param3.Size = new System.Drawing.Size(55, 21);
             this.label_param3.TabIndex = 1;
@@ -259,8 +180,8 @@
             // label_param4
             // 
             this.label_param4.AutoSize = true;
-            this.label_param4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_param4.Location = new System.Drawing.Point(494, 165);
+            this.label_param4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_param4.Location = new System.Drawing.Point(494, 179);
             this.label_param4.Name = "label_param4";
             this.label_param4.Size = new System.Drawing.Size(55, 21);
             this.label_param4.TabIndex = 1;
@@ -269,8 +190,8 @@
             // label_param5
             // 
             this.label_param5.AutoSize = true;
-            this.label_param5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label_param5.Location = new System.Drawing.Point(644, 165);
+            this.label_param5.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_param5.Location = new System.Drawing.Point(644, 179);
             this.label_param5.Name = "label_param5";
             this.label_param5.Size = new System.Drawing.Size(55, 21);
             this.label_param5.TabIndex = 1;
@@ -293,24 +214,16 @@
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Location = new System.Drawing.Point(10, 11);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(750, 460);
+            this.panel1.Size = new System.Drawing.Size(750, 498);
             this.panel1.TabIndex = 0;
             // 
             // BigScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 562);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button_select);
-            this.Controls.Add(this.textBox_pic_num);
-            this.Controls.Add(this.textBox_page_num);
-            this.Controls.Add(this.label_page);
-            this.Controls.Add(this.button_next);
-            this.Controls.Add(this.button_pre);
+            this.ClientSize = new System.Drawing.Size(784, 609);
             this.Controls.Add(this.panel1);
             this.Name = "BigScreen";
             this.Text = "BigScreen";
@@ -323,20 +236,11 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button_pre;
-        private System.Windows.Forms.Button button_next;
-        private System.Windows.Forms.Label label_page;
-        private System.Windows.Forms.TextBox textBox_page_num;
-        private System.Windows.Forms.Button button_select;
-        private System.Windows.Forms.TextBox textBox_pic_num;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
--- FenXingFormDemo/ConsoleScreen.cs
+++ FenXingFormDemo/ConsoleScreen.cs
@@ -11,6 +11,8 @@ namespace FenXingFormDemo
 {
     public partial class ConsoleScreen : Form
     {
+        private const int const_page_num = 1;
+        private const int const_pages = 3;
         private BigScreen m_big_screen;//大屏程序
 
         public ConsoleScreen()
@@ -26,7 +28,7 @@ namespace FenXingFormDemo
              * *******--A3
              * *******-B1等
              */
-            m_big_screen = new BigScreen(1, 3);
+            m_big_screen = new BigScreen(const_page_num, const_pages);
             m_big_screen.InitFxImageList();
         }
 
@@ -37,10 +39,60 @@ namespace FenXingFormDemo
          * */
         private void button_bigscreen_Click(object sender, EventArgs e)
         {
-
             m_big_screen.Show();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_big_screen.button_pre_Click();
+            UpdatePageAndButtonEnableStatus();
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m_big_screen.button_next_Click();
+            UpdatePageAndButtonEnableStatus();
+        }
+
+        private void UpdatePageAndButtonEnableStatus()
+        {
+            int currentPage = m_big_screen.GetCurrentPageIndex();
+            BigScreenPageInfo.Text = "第" + currentPage + "页";
+
+            if(currentPage <= 1)
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+
+            if (currentPage >= const_pages)
+            {
+                button2.Enabled = false;
+            }
+            else
+            {
+                button2.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int page_num = 1;
+            int pic_num = 1;
+            if(!Int32.TryParse(PageNumTextBox.Text.Trim(), out page_num))
+            {
+                MessageBox.Show("请输入正确的页码值.");
+                return;
+            }
+            if (!Int32.TryParse(PicNumTextBox.Text.Trim(), out pic_num))
+            {
+                MessageBox.Show("请输入正确的图片索引.");
+                return;
+            }
+            m_big_screen.button_select_Click(page_num, pic_num);
+        }
     }
 }
